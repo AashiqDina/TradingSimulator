b707d1deb80d2add6b6dbd0214c7ae2898779c97
AashiqDina <98742552+AashiqDina@users.noreply.github.com>
2025-02-03 00:52:31 +0000

    Renamed Folder - v0.55

--- TradingSimulator-backend/obj/Debug/net9.0/TradingSimulatorAPI.AssemblyInfo.cs
+++ TradingSimulator-backend/obj/Debug/net9.0/TradingSimulatorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingSimulatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057b3194699dd903e047d057803a14630fb6374b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fcf0ef79382c8c9c55350e7161d4e7834c55542")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingSimulatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingSimulatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
