ea1cd28dcb684dbcbdecda31fe35e54e09cab8ca
AashiqDina <98742552+AashiqDina@users.noreply.github.com>
2025-04-14 01:31:12 +0100

    Did the About page - Havent been able to do much lately due to some personal matters (hope to be able to do more soon)

--- trading-simulator-frontend/src/About.tsx
+++ trading-simulator-frontend/src/About.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='AboutContainer'>
+      <h1>About TradeSim</h1>
+      <p className='AboutText'>This Stock Trading Simulator is a simplified implementation of a stock portfolio management system designed to replicate 
+          investment functionalities to give users the opportunity to learn how to invest. I developed this project as a practical learning exercise to 
+         strengthen my understanding of the technologies applied within the company I am pursuing.
+      </p>
     </div>
   );
 };
